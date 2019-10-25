986ea784bd4c390fcf2f06d557e2fad84ab7cf6a
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-10-25 09:49:23 +0530

    brand-html5-app-landing-page-responsive-web-template

---
+++ brand-html5-app-landing-page-responsive-web-template/contactthanks.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<title>Your message has been sent!</title>
+	
+</head>
+
+<body>
+		
+	<h1>Your message has been sent!</h1>
+
+</body>
+</html>
