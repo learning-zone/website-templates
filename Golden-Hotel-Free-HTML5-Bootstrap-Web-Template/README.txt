5794f1f3e639d85cebf64b1583f67732a4d6d103
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-10-25 10:00:11 +0530

    Golden-Hotel-Free-HTML5-Bootstrap-Web-Template

---
+++ Golden-Hotel-Free-HTML5-Bootstrap-Web-Template/README.txt
@@ -0,0 +1,32 @@
+Bootstrap/HTML5 Responsive Template
+
+Golden Hotel free html5 bootstrap web template is a simple clean modern HTML5 Bootstrap template. This template fits perfectly for the Hotels, restaurants, pubs, bars, wineries and other companies offering foods and drinks. This template designed with height quality standards to meet the latest requirment and it is a responsive template fits in all devices with multi browser support. This theme developed using latest modern technology like Bootstrap, html5 and css3 which is very easy to customeise the theme as per your requirements. Get it now download for free.  
+
+Credits :
+-------
+=> Design and developed: "WebThemez"  http://webthemez.com
+=> Photos used in template: **Unsplash** - http://unsplash.com
+=> For more free web themes: http://webthemez.com
+=> Framework : http://getbootstrap.com
+
+Important Note:
+---------------
+To remove backlink from the template, you need to donate to remove the backlink from the template.
+Any question contact us: webthemez@gmail.com
+
+
+License :
+-------
+**Creative Commons Attribution 3.0** - http://creativecommons.org/licenses/by/3.0/
+
+- You are allowed to use all files for both personal and commercial projects.
+
+- If you use/modify the resources in your projects,we’d appreciate a linkback to this site.
+
+- You do not have rights to redistribute,resell or offer files from this site to any third party
+
+- If you wish to remove backlink from the template, you need to donate min USD $10 to remove backlink (credits) form the template
+
+- If you have any question,feel free to contact us at webthemez@gmail.com
+
+- All images user here is for demo purpose only, we are not responsible for any copyrights.
---
+++ Golden-Hotel-Free-HTML5-Bootstrap-Web-Template/js/custom-scripts.js
@@ -0,0 +1,118 @@
+/*
+Author: webthemez.com
+Author URL: http://webthemez.com
+*/
+jQuery(function($) {
+    'use strict';
+     
+    $(window).scroll(function(event) {
+        Scroll();
+    });
+
+    $('.navbar-collapse ul li a').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 80
+        }, 1000);
+        return false;
+    });
+ 
+    function Scroll() {
+        var contentTop = [];
+        var contentBottom = [];
+        var winTop = $(window).scrollTop();
+        var rangeTop = 200;
+        var rangeBottom = 500;
+        $('.navbar-collapse').find('.scroll a').each(function() {
+            contentTop.push($($(this).attr('href')).offset().top);
+            contentBottom.push($($(this).attr('href')).offset().top + $($(this).attr('href')).height());
+        })
+        $.each(contentTop, function(i) {
+            if (winTop > contentTop[i] - rangeTop) {
+                $('.navbar-collapse li.scroll')
+                    .removeClass('active')
+                    .eq(i).addClass('active');
+            }
+        })
+    };
+
+    $('#tohash').on('click', function() {
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 5
+        }, 1000);
+        return false;
+    });
+
+  
+    new WOW().init();
+     
+    smoothScroll.init();
+
+    
+    $(window).load(function() {
+        'use strict';
+        var $portfolio_selectors = $('.portfolio-filter >li>a');
+        var $portfolio = $('.portfolio-items');
+        $portfolio.isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+        });
+
+        $portfolio_selectors.on('click', function() {
+            $portfolio_selectors.removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $portfolio.isotope({
+                filter: selector
+            });
+            return false;
+        });
+    });
+
+    $(document).ready(function() {
+   
+        $.fn.animateNumbers = function(stop, commas, duration, ease) {
+            return this.each(function() {
+                var $this = $(this);
+                var start = parseInt($this.text().replace(/,/g, ""));
+                commas = (commas === undefined) ? true : commas;
+                $({
+                    value: start
+                }).animate({
+                    value: stop
+                }, {
+                    duration: duration == undefined ? 1000 : duration,
+                    easing: ease == undefined ? "swing" : ease,
+                    step: function() {
+                        $this.text(Math.floor(this.value));
+                        if (commas) {
+                            $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+                        }
+                    },
+                    complete: function() {
+                        if (parseInt($this.text()) !== stop) {
+                            $this.text(stop);
+                            if (commas) {
+                                $this.text($this.text().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,"));
+                            }
+                        }
+                    }
+                });
+            });
+        };
+
+        $('.business-stats').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+            var $this = $(this);
+            if (visible) {
+                $this.animateNumbers($this.data('digit'), false, $this.data('duration'));
+                $this.unbind('inview');
+            }
+        });
+    });
+
+ 
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+        social_tools: false
+    });
+ 
+
+});
