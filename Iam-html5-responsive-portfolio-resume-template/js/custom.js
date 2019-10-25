86c4622e76ac9674893efd0d0ddf9a5667fe3118
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-10-25 15:56:00 +0530

    Iam-html5-responsive-portfolio-resume-template

---
+++ Iam-html5-responsive-portfolio-resume-template/js/custom.js
@@ -0,0 +1,58 @@
+/*
+  * @package 
+  * @subpackage template name HTML
+  * 
+  * Template Scripts
+  * Created by themeturn
+  
+  1. Fixed header
+  2. Site search
+  3. Main slideshow
+  4. Owl Carousel
+      a. Testimonial
+      b. Clients
+      c. Team
+  5. Back to top
+  6. Skills
+  7. BX slider
+      a. Blog Slider
+      b. Portfolio item slider
+  8. Isotope
+  9. Animation (wow)
+  10. Flickr
+  
+*/
+
+
+jQuery(function($) {
+  "use strict";
+
+   $('.navigation').singlePageNav({
+        currentClass: 'active',
+        changeHash: true,
+        scrollSpeed: 750,
+        offset: 0,
+        filter: ':not(.external)',
+        easing: 'swing',
+
+    });
+
+    $.noConflict();
+     $('.nav a').on('click', function(){ 
+        if($('.navbar-toggle').css('display') !='none'){
+            $(".navbar-toggle").trigger( "click" );
+        }
+    });
+
+
+ 
+// prettyphoto
+
+ $("a[data-rel^='prettyPhoto']").prettyPhoto();
+ 
+    
+ 
+
+
+
+});
