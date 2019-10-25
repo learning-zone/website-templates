216fdaed5535cd7ecc5ba8bd889f8b5155313921
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-10-25 09:44:33 +0530

    ace-responsive-coming-soon-template

---
+++ ace-responsive-coming-soon-template/js/global.js
@@ -0,0 +1,40 @@
+/*
+Theme by: WebThemez.com
+Note: Please use our back link in your site
+*/
+$( function() {
+	$.vegas( 'slideshow', {
+		delay: 8000,
+		backgrounds: [
+			{ src: 'images/background4.jpg', fade: 4000 },
+			{ src: 'images/background3.jpg', fade: 4000 },
+			{ src: 'images/background2.jpg', fade: 4000 },
+			{ src: 'images/background1.jpg', fade: 4000 }
+		]
+	} )( 'overlay' );
+	
+        var endDate = "December  27, 2014 15:03:25";
+
+        $('.countdown.simple').countdown({ date: endDate });
+
+        $('.countdown.styled').countdown({
+          date: endDate,
+          render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+          }
+        });
+
+        $('.countdown.callback').countdown({
+          date: +(new Date) + 10000,
+          render: function(data) {
+            $(this.el).text(this.leadingZeros(data.sec, 2) + " sec");
+          },
+          onEnd: function() {
+            $(this.el).addClass('ended');
+          }
+        }).on("click", function() {
+          $(this).removeClass('ended').data('countdown').update(+(new Date) + 10000).start();
+        });
+
+      });
+   
